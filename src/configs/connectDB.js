299a76a78636fa04cbfd75b97db59d9b029fcf7f
dinhtuannam <namdinhtuan5@gmail.com>
2023-05-19 22:49:15 +0700

    test

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,10 +1,12 @@
 import mysql from 'mysql2/promise';
+require('dotenv').config();
 
 // create the connection to database
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'netflix'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME
 });
 
 export default pool;
